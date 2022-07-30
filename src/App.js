6b464317c62cae113f8aa9a993f49115479034fb
selcuklu <yilmax4@gmail.com>
2022-07-30 18:10:34 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 function App() {
-  return <div className="App"></div>;
+  return(
+  <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+    </BrowserRouter>
+  </>
+  )
 }
 
 export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { HeaderText, HomeImg, ImgDiv } from "./Dashboard.style"
+const Dashboard = () => {
+  return (
+    <ImgDiv >Dashboard</ImgDiv>
+  )
+}
+
+export default Dashboard
